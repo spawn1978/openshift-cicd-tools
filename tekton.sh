c91788b0463d960949270c97f8774fb00ac1ec4c
spawn1978 <leguizamoncarlos@gmail.com>
2021-03-12 23:48:21 -0300

    Update tekton.sh

--- tekton.sh
+++ tekton.sh
@@ -2,3 +2,6 @@
 oc new-project tekton-pipelines --display-name='Tekton Pipelines'
 oc adm policy add-scc-to-user anyuid -z tekton-pipelines-controller
 oc apply --filename https://storage.googleapis.com/tekton-releases/latest/release.yaml
+
+oc apply -f echo-hello-world-task.yaml
+oc apply -f echo-hello-world-task-run.yaml
